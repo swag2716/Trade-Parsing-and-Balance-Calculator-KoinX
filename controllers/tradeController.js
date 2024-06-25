d81443da3c7de5eea85886dc7415781bc95fd185
SwapnikaAg06 <swapnika@fourie.ai>
2024-06-25 15:12:26 +0530

    upload CSV and save the data in database

---
+++ controllers/tradeController.js
@@ -0,0 +1,38 @@
+const Trade = require("../models/trade");
+const csv = require("csv-parser");
+const fs = require("fs");
+
+exports.uploadCSV = (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("No files were uploaded.");
+  }
+
+  const results = [];
+
+  fs.createReadStream(req.file.path)
+    .pipe(csv())
+    .on("data", (data) => results.push(data))
+    .on("end", () => {
+      results.forEach((row) => {
+        const [BaseCoin, QuoteCoin] = row.Market.split("/");
+        // const utcTime = moment(row.UTC_Time).format('YYYY-MM-DD HH:mm:ss');
+        const trade = new Trade({
+          User_Id: row.User_ID,
+          UTC_Time: new Date(row.UTC_Time),
+          Operation: row.Operation,
+          Market: row.Market,
+          BaseCoin,
+          QuoteCoin,
+          BuySellAmount: parseFloat(row["Buy/Sell Amount"]),
+          Price: parseFloat(row.Price),
+        });
+
+        trade
+          .save()
+          .then(() => console.log("Trade saved to database"))
+          .catch((err) => console.error(err));
+      });
+
+      res.send("File uploaded and data stored successfully.");
+    });
+};

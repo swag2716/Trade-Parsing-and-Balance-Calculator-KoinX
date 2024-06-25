b794211c266aa9e46a210497c113a612e3049789
SwapnikaAg06 <swapnika@fourie.ai>
2024-06-25 15:19:04 +0530

    Changes schema to include user_id

--- models/trade.js
+++ models/trade.js
@@ -1,24 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const tradeSchema = new Schema({
+const tradeSchema = new Schema(
+  {
+    User_Id: String,
     UTC_Time: Date,
     Operation: String,
     Market: {
-        type: String,
-        validate: {
-            validator: function(v) {
-                return v.split('/').length === 2;
-            },
-            message: props => `${props.value} is not in the format "base/quote"!`
-        }
+      type: String,
+      validate: {
+        validator: function (v) {
+          return v.split("/").length === 2;
+        },
+        message: (props) => `${props.value} is not in the format "base/quote"!`,
+      },
     },
     BaseCoin: String,
     QuoteCoin: String,
     BuySellAmount: Number,
-    Price: Number
-});
+    Price: Number,
+  },
+  { versionKey: false }
+);
 
-const Trade = mongoose.model('Trade', tradeSchema);
+const Trade = mongoose.model("Trade", tradeSchema);
 
 module.exports = Trade;
